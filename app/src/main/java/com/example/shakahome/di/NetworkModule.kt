28144125fd71b66c231396f0ddf33746fafe5273
hamuyatti <syota.takarabe@gmail.com>
2022-05-21 08:16:24 +0900

    retrofitを提供するためのhiltの記述を追加

--- app/src/main/java/com/example/shakahome/di/NetworkModule.kt
+++ app/src/main/java/com/example/shakahome/di/NetworkModule.kt
@@ -1,10 +1,15 @@
 package com.example.shakahome.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
@@ -14,5 +19,19 @@ object NetworkModule {
     @Singleton
     fun providesOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder().build()
+    }
+
+    @ExperimentalSerializationApi
+    @Provides
+    @Singleton
+    fun providesRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        val contentType = "application/json".toMediaType()
+        val format = Json { ignoreUnknownKeys = true }
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(format.asConverterFactory(contentType))
+            .build()
     }
 }
