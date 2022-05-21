53c62be819385185305ae7339e541765662eeeb1
hamuyatti <syota.takarabe@gmail.com>
2022-05-21 11:08:14 +0900

    viewModelにfetchStreamerInfoを作成

--- presentation/viewmodel/src/main/java/com/example/viewmodel/StreamerInfoUiState.kt
+++ presentation/viewmodel/src/main/java/com/example/viewmodel/StreamerInfoUiState.kt
@@ -3,6 +3,7 @@ package com.example.viewmodel
 import com.example.model.StreamerInfo
 
 sealed interface StreamerInfoUiState{
+    object Empty : StreamerInfoUiState
     object IsLoading : StreamerInfoUiState
     data class Success(val streamerInfo: StreamerInfo) : StreamerInfoUiState
     data class Error(val throwable: Throwable)
--- presentation/viewmodel/src/main/java/com/example/viewmodel/StreamerInfoViewModel.kt
+++ presentation/viewmodel/src/main/java/com/example/viewmodel/StreamerInfoViewModel.kt
@@ -1,13 +1,31 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.example.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.usecase.FetchStreamerInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class StreamerInfoViewModel @Inject constructor(
     private val useCase: FetchStreamerInfoUseCase
 ) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(StreamerInfoUiState.Empty)
+    val uiState = _uiState.asStateFlow()
+
+    fun fetchStreamerInfo(){
+        runCatching {
+            viewModelScope.launch {
+                val result = useCase.invoke()
+            }
+        }
+    }
 
 }
