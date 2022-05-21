13321385220d9833022f8de143cf9e64b97ba460
hamuyatti <syota.takarabe@gmail.com>
2022-05-21 10:56:50 +0900

    Streamerの情報を表示する画面の状態を表すStreamerInfoUiStateを作成

---
+++ presentation/viewmodel/src/main/java/com/example/viewmodel/StreamerInfoUiState.kt
@@ -0,0 +1,11 @@
+package com.example.viewmodel
+
+import com.example.model.StreamerInfo
+
+sealed interface StreamerInfoUiState{
+    object IsLoading : StreamerInfoUiState
+    data class Success(val streamerInfo: StreamerInfo) : StreamerInfoUiState
+    data class Error(val throwable: Throwable)
+}
+
+
--- presentation/viewmodel/src/main/java/com/example/viewmodel/StreamerInfoViewModel.kt
+++ presentation/viewmodel/src/main/java/com/example/viewmodel/StreamerInfoViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.usecase.FetchStreamerInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class StreamerInfoViewModel @Inject constructor(
-
+    private val useCase: FetchStreamerInfoUseCase
 ) : ViewModel() {
+
 }
