542cb1acc22ddc94a0fac1542d76ebd60b30d661
hamuyatti <syota.takarabe@gmail.com>
2022-05-22 23:37:40 +0900

    ShakaHomeTopLevelNavigationの作成

--- app/src/main/java/com/example/shakahome/navigation/ShakaHomeNavHost.kt
+++ app/src/main/java/com/example/shakahome/navigation/ShakaHomeNavHost.kt
@@ -7,16 +7,23 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.ui.navigation.ReportDestination
+import com.example.ui.navigation.infoGraph
+import com.example.ui.navigation.reportGraph
 
 
 @Composable
 fun ShakaHomeNavHost(
-    windowSizeClass : WindowSizeClass,
+    windowSizeClass: WindowSizeClass,
     modifier: Modifier = Modifier,
-    navController : NavHostController = rememberNavController(),
-    startDestination : String = ReportDestination.route
-){
-    NavHost(navController = navController, startDestination = startDestination, modifier = modifier){
-
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ReportDestination.route
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        reportGraph(windowSizeClass = windowSizeClass)
+        infoGraph(windowSizeClass = windowSizeClass)
     }
 }
