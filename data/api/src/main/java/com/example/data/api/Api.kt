07be740ecfd2da4f22022fbc45a7f6bd2ce985c1
shota.takarabe <syota.takarabe@gmail.com>
2022-06-18 03:00:53 +0900

    フォロー情報を表すFollowInfoを作成

--- data/api/src/main/java/com/example/data/api/Api.kt
+++ data/api/src/main/java/com/example/data/api/Api.kt
@@ -1,5 +1,6 @@
 package com.example.data.api
 
+import com.example.model.FollowInfo
 import com.example.model.StreamerInfo
 import retrofit2.Response
 import retrofit2.http.GET
@@ -16,4 +17,14 @@ interface Api {
     fun fetchStreamerInfo(
         @Query("login") StreamerId: Int = 49207184
     ): Response<StreamerInfo>
+
+    @Headers(
+        "Authorization: Bearer 0vvi1ikhyic6c2rq8flw1ukwp0pb2w",
+        "Client-Id:x24r8nw8hd6arlvf1hhjuic2n154fl"
+    )
+    @GET("users")
+    fun fetchStreamerFollowInfo(
+        @Query("from_id") StreamerId: Int = 49207184
+    ): Response<FollowInfo>
+
 }
---
+++ model/src/main/java/com/example/model/FollowInfo.kt
@@ -0,0 +1,21 @@
+package com.example.model
+
+data class FollowInfo(
+    val `data`: List<Data>,
+    val pagination: Pagination,
+    val total: Int
+)
+
+data class Data(
+    val followed_at: String,
+    val from_id: String,
+    val from_login: String,
+    val from_name: String,
+    val to_id: String,
+    val to_login: String,
+    val to_name: String
+)
+
+data class Pagination(
+    val cursor: String
+)
