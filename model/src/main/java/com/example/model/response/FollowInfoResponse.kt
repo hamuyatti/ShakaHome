e031c16e1aa7d4eb486fa0ad8320cef201efbdc4
shota.takarabe <syota.takarabe@gmail.com>
2022-08-07 19:34:12 +0900

    修正

--- model/src/main/java/com/example/model/response/FollowInfoResponse.kt
+++ model/src/main/java/com/example/model/response/FollowInfoResponse.kt
@@ -1,5 +1,6 @@
 package com.example.model.response
 
+import com.example.core.util.DateUtil.utcToJtc
 import com.example.model.domain.FollowInfo
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -7,7 +8,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class FollowInfoResponse(
     val `data`: List<EachFollowInfo>,
-    val pagination: PastVideosPagination,
+    val pagination: FollowInfoPagination,
     val total: Int
 )
 
@@ -27,42 +28,13 @@ data class FollowInfoPagination(
     val cursor: String
 )
 
-
 fun FollowInfoResponse.asDomainModel(): FollowInfo {
     return FollowInfo(
-        FollowsInfo = this.data,
-        total = this.total.toString()
-    )
-}
-
-private fun dummyData(): FollowInfoResponse {
-    return FollowInfoResponse(
-        total = 163,
-        data = listOf(
-            EachFollowInfo(
-                fromId = "49207184",
-                fromLogin = "fps_shaka",
-                fromName = "fps_shaka",
-                toId = "167077641",
-                toLogin = "sovault",
-                toName = "ソバルト",
-                followedAt = "2022-06-13T02:02:32Z"
-            ),
-            EachFollowInfo(
-                fromId = "49207184",
-                fromLogin = "fps_shaka",
-                fromName = "fps_shaka",
-                toId = "190835892",
-                toLogin = "lck_korea",
-                toName = "LCK_Korea",
-                followedAt = "2022-04-02T12:27:43Z"
+        FollowsInfo = this.data.map {
+            it.copy(
+                followedAt = utcToJtc(it.followedAt)
             )
-        ),
-        pagination = PastVideosPagination(
-            cursor = "eyJiIjpudWxsLCJhIjp7IkN1cnNvciI6ImV5SjBjQ0k2SW5WelpYSTZORGt5TURje" +
-                    "E9EUTZabTlzYkc5M2N5SXNJblJ6SWpvaWRYTmxjam8wTXpZMk9EZzVNVFVpTENKcGND" +
-                    "STZJblZ6WlhJNk5Ea3lNRGN4T0RRNlptOXNiRzkzY3lJc0ltbHpJam9pTVRZME5EZzJ" +
-                    "NRGd6TVRjMk1EWXhPREU0TUNKOSJ9fQ"
-        )
+        },
+        total = this.total.toString()
     )
 }
