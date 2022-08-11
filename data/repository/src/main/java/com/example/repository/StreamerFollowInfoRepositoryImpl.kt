6aa1873664fc89ea0595612ec52aaad46e21bce4
hamuyatti <syota.takarabe@gmail.com>
2022-08-11 16:42:25 +0900

    ローカルのデータのためのhilt moduleを作成

--- data/repository/src/main/java/com/example/repository/StreamerFollowInfoRepositoryImpl.kt
+++ data/repository/src/main/java/com/example/repository/StreamerFollowInfoRepositoryImpl.kt
@@ -1,13 +1,15 @@
 package com.example.repository
 
 import com.example.data.api.StreamerFollowInfoRemoteDataSource
+import com.example.db.FollowLocalDataSource
 import com.example.irepository.StreamerFollowInfoRepository
 import com.example.model.response.FollowInfoResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class StreamerFollowInfoRepositoryImpl(
-    private val remoteDataSource: StreamerFollowInfoRemoteDataSource
+    private val remoteDataSource: StreamerFollowInfoRemoteDataSource,
+    private val localDataSource: FollowLocalDataSource
 ) : StreamerFollowInfoRepository {
     override suspend fun fetchStreamerFollowInfo(): FollowInfoResponse =
         withContext(Dispatchers.IO) {
