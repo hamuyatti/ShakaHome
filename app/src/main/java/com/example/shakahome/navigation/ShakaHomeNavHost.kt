8b0722e20a3bfec2432747f569bae4c55faeaaea
hamuyatti <syota.takarabe@gmail.com>
2022-09-26 11:15:29 +0900

    不要な引数の削除

--- app/src/main/java/com/example/shakahome/navigation/ShakaHomeNavHost.kt
+++ app/src/main/java/com/example/shakahome/navigation/ShakaHomeNavHost.kt
@@ -1,20 +1,18 @@
 package com.example.shakahome.navigation
 
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.example.ui.feature.drawer.settings.settingGraph
 import com.example.ui.navigation.ReportDestination
 import com.example.ui.navigation.infoGraph
 import com.example.ui.navigation.reportGraph
+import com.example.ui.navigation.settingGraph
 
 
 @Composable
 fun ShakaHomeNavHost(
-    windowSizeClass: WindowSizeClass,
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
     startDestination: String = ReportDestination.route,
@@ -24,12 +22,8 @@ fun ShakaHomeNavHost(
         startDestination = startDestination,
         modifier = modifier
     ) {
-        reportGraph(
-            windowSizeClass = windowSizeClass,
-        )
-        infoGraph(
-            windowSizeClass = windowSizeClass,
-        )
+        reportGraph()
+        infoGraph()
         settingGraph()
     }
 }
